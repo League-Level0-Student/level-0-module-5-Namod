762b17769081651ae2ee0ca6ae4b0637dcb37f48
League Student <league@iMac-13.attlocal.net>
2019-07-21 11:24:33 -0700

    smile:

---
+++ src/test.java
@@ -0,0 +1,13 @@
+
+public class test {
+public static void main(String[] args) {
+	 String name = "Thadius";
+	 
+	 if(name == "Thadius"){
+	   System.out.println("Axlerod");
+	 }else{
+	   System.out.println("Marbrand");
+	 }
+
+}
+}
